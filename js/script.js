4906be07836e53f4165e9de7717d11a3e954299e
Ahmadkabiri <Ahmadkabiriw@gmail.com>
2022-11-25 09:40:52 +0330

    for loop is like this 1

--- js/script.js
+++ js/script.js
@@ -1,32 +1,3 @@
-let mark = 15 ;
-
-switch(mark){
-    case 18 :
-    case 19 :
-    case 20 :
-        alert("Level A");
-        break ;
-    case 15 :
-    case 16 :
-    case 17 :
-        alert("Level B");
-        break ;
-    case 12 :
-    case 13 :
-    case 14 :
-        alert("Level C");
-        break ;
-    default :
-        alert("You are fierd");
-        break ;
-}
-
-if(mark>=18 && mark<=20){
-    alert("level A")
-}else if(mark>=15 && mark<=17){
-    alert("level B") ;
-}else if(mark>=12 && mark<=14){
-    alert("level C") ;
-}else{
-    alert(" you are fired")
+for(let i=0 ; i<10 ; i++){
+    console.log("yes" , i)
 }

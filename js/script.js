46e7d92790bcbda39b4c23c4c7fd673958159102
Ahmadkabiri <Ahmadkabiriw@gmail.com>
2022-11-25 10:32:48 +0330

    for while do-while works like this :))

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-for(let i=0 ; i<10 ; i++){
-    console.log("yes" , i)
+for(let i=0 ; i<=100 ; i++){
+    if(i%2 !== 0) continue ;
+    console.log(i);
 }
+
+let i = 0 ;
+while (i<101){
+    if(i%2 == 0){
+        console.log(i);
+    }
+    i++ ;
+}
+
+let j = 0 ;
+do{
+    if(j%2 == 0){
+        console.log(j);
+    }
+    j++ ; 
+}while(j<101);

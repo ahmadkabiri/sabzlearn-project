db98bbe6fb2f49e9f1ff2492dce638e50aec81a1
Ahmadkabiri <Ahmadkabiriw@gmail.com>
2022-11-27 10:39:41 +0330

    cart code

--- js/script.js
+++ js/script.js
@@ -1,28 +1,42 @@
-const users = [
-    { firstName : "Ahmad" , lastName : "Kabiri" , age : 24 , email : "Ahmadkabiriw@gmail.com" },
-    { firstName : "Ali" , lastName : "Kabiri" , age : 14 , email : "Alikabiri1386@gmail.com"},
-    { firstName : "Abbas" , lastName : "Kabiri" , age : 30 , email : "Abbaskabiri1371@gmail.com"},
-];
+const products = [
+{ id : 1 , name : "book" },
+{ id : 2 , name : "pen" },
+{ id : 3 , name : "pencil" },
+{ id : 4 , name : "roler" },
+{ id : 5 , name : "eraser" },
+{ id : 6 , name : "paper" },
+]
 
-let userObject = {};
+const userBought = [
+    { id : 7 , name : "tarash" },
+    { id : 8 , name : "notebook" },
+    { id : 9 , name : "bag" },
+]
 
-while(true){
+let userTold = prompt(`Do you want to delete an item of your cart?(Enter delete !)
+Or do you want to buy a something else ?(Enter add !)`)
 
-    userObject.firstName = prompt("Enter your first name !");
-    if(userObject.firstName.length>=3 && userObject.firstName.length <= 10){
-        break;
+if(userTold == "delete"){
+    let userDelete = prompt("what you want to delete?");
+    let itemIndex = userBought.findIndex((item)=>{
+        return item.name == userDelete ;
+    })
+    if(itemIndex == -1){
+        alert("This product is not in your cart !!")
+    }else{
+        userBought.splice(itemIndex,1);
+        console.log(userBought)
+    }
+}else{
+    let useradd = prompt("what you want to add?");
+    let itemIIndexadded = products.findIndex((item)=>{
+        return item.name == useradd ;
+    })
+    if(itemIIndexadded == -1){
+        alert("This product is not in our shop !!")
+    }else{
+        userBought.push(products[itemIIndexadded]);
+        console.log(userBought);
+        console.group(userBought);
     }
 }
-
-while(true){
-
-    userObject.lastName = prompt("Enter your last name !");
-    if(userObject.lastName.length>=3 && userObject.lastName.length <= 15){
-        break;
-    }
-}
-
-userObject.age = prompt("Enter your age !");
-userObject.email = prompt("Enter your email !");
-users[3] = userObject ;
-

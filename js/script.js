40cfb6bfa73e301551064ca613dd2d8f97feb53b
Ahmadkabiri <Ahmadkabiriw@gmail.com>
2022-11-28 11:38:39 +0330

    todo in js code

--- js/script.js
+++ js/script.js
@@ -1,29 +1,52 @@
-const products =[
-    {name : "pen" , price : 12*1000} ,
-    {name : "pencil" , price : 10*1000} ,
-    {name : "book" , price : 115*1000} ,
-    {name : "watch" , price : 450*1000} ,
-    {name : "bag" , price : 80*1000} ,
-    {name : "phone" , price : 4000*1000} ,
-    {name : "laptop" , price : 20000*1000} ,
+const toDoList =[
+    { toDo : "Going to park" , isDO : true},
+    { toDo : "working" , isDO : false},
+    { toDo : "Going to GYM" , isDO : false},
+    { toDo : "Going to mousque" , isDO : false},
+    { toDo : "studing" , isDO : true},
+    { toDo : "Going to bed" , isDO : false},
+    { toDo : " Call to my boss" , isDO : true},
 ]
 
-// const productsUp100 = products.filter((item)=>{
-//     return item.price < 100*1000
+let userRequest = Number(prompt(`1. Add todo
+2. remove todo
+3. Todo is done`));
+
+
+ if( userRequest===1 ){
+    let whatToDo = prompt("Add your toDo !")
+    let newToDo = { toDo : whatToDo , isDO : false}
+    toDoList.push(newToDo);
+    // console.log(toDoList);
+ }else if ( userRequest === 2){
+    let removeToDo = prompt("کدوم تودو رو حذف کنم؟")
+    const newFilteredToDo = toDoList.filter(el => {
+        return el.toDo !== removeToDo
+    })
+    console.log(newFilteredToDo);
+ }else if ( userRequest === 3 ) {
+     let newStatus = prompt("کدوم تودو رو انجام دادی؟")
+    // const didNotToDo = toDoList.filter(el => {
+    //     return !el.isDO
+    // })
+    const toDoIndex = toDoList.findIndex(item => {
+        return item.toDo === newStatus ;
+    })
+    toDoList[toDoIndex].isDO = true ;
+    console.log(toDoList);
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+// toDoList.forEach(el => {
+//     console.log(" You did : " , el.toDo)
 // })
-
-// let postPrice = productsUp100.length*1000 ;
-
-// let totalPrice = 0 ;
-// products.forEach(element => {
-//     totalPrice += element.price ;
-// });
-
-// console.log(`Your product price is : ${ totalPrice} toman 
-// your post price is : ${postPrice} toman
-// And your total price is : ${totalPrice+postPrice}`)
-
-
-const someOfProducts = products.splice(2,3);
-console.log(someOfProducts);
-console.log(products)
